8779fc342450c377a9255cda17ee81e5c7dee414
paulohdelia <paulohdelia@gmail.com>
2020-09-04 12:37:31 -0300

    feat: add service to create user

---
+++ backend/src/repositories/IUserRepository.ts
@@ -0,0 +1,7 @@
+import CreateUserDTO from '../dtos/CreateUserDTO';
+import User from '../models/User';
+
+export default interface IUserRepository {
+  findByEmail(email: string): Promise<User | undefined>;
+  create(createUserDTO: CreateUserDTO): Promise<User>;
+}
---
+++ backend/src/repositories/UserRepository.ts
@@ -0,0 +1,34 @@
+import { getRepository, Repository } from 'typeorm';
+import CreateUserDTO from '../dtos/CreateUserDTO';
+import User from '../models/User';
+import IUserRepository from './IUserRepository';
+
+class UserRepository implements IUserRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne({
+      where: { email },
+    });
+
+    return user;
+  }
+
+  public async create({ name, email, password }: CreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UserRepository;
