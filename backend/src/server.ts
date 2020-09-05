abaae452476a9d2aa7c82e1f286565de7f99afb7
paulohdelia <paulohdelia@gmail.com>
2020-09-05 00:10:45 -0300

    feat: add class to handle errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,23 @@
 import 'reflect-metadata';
-import express from 'express';
 import './database';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3333);
