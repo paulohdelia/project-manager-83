81680b77e5c43e959cfb743d8162a30c523ab061
paulohdelia <paulohdelia@gmail.com>
2020-10-08 17:03:50 -0300

    add links to the sign-in pages

--- frontend/src/pages/Start/index.tsx
+++ frontend/src/pages/Start/index.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
+import { useHistory } from 'react-router-dom';
 import { FaCode, FaUsers } from 'react-icons/fa';
 import { Container, EntryCard, EntryCardItem } from './styles';
 
 import logo from '../../assets/logo.png';
 
 const Start: React.FC = () => {
+  const history = useHistory();
+
+  const navigate = useCallback((path: string) => {
+    history.push(`/sign-in/${path}`);
+  }, [history]);
+
   return (
     <Container>
       <img src={logo} alt="Project Manager" />
       <EntryCard>
-        <EntryCardItem>
+        <EntryCardItem onClick={() => navigate('dev')}>
           <FaCode size={50} />
           <span>Entrar como Dev</span>
         </EntryCardItem>
-        <EntryCardItem color="#fff" background="#111">
+        <EntryCardItem onClick={() => navigate('client')} color="#fff" background="#111">
           <FaUsers size={50} />
           <span>Entrar como Cliente</span>
         </EntryCardItem>
