cd2abfc5244744961186a7ed92d04d55f7f6f1b2
paulohdelia <paulohdelia@gmail.com>
2020-10-10 18:42:51 -0300

    allow cross-origin requests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,22 @@
-import 'reflect-metadata';
-import './config/env';
-import 'express-async-errors';
-import './database';
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import 'reflect-metadata';
+import cors from 'cors';
+
+import './config/env';
+
+import './database';
 import routes from './routes';
 import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
+    console.error(err);
     return response
       .status(err.statusCode)
       .json({ status: 'error', message: err.message });
