3f379d4be539b201e919f66ce19e6385d5c4a553
paulohdelia <paulohdelia@gmail.com>
2020-10-10 18:44:08 -0300

    add package react notifications component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Routes from './routes';
+import ReactNotification from 'react-notifications-component';
 
+import Routes from './routes';
 import GlobalStyles from './styles/global';
+
+import 'react-notifications-component/dist/theme.css';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <ReactNotification />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
